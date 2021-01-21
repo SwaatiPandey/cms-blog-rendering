267ed65f507f6c34b0405f19eba856d2a0270f96
SwaatiPandey <swati.pandey@contentstack.com>
2021-01-21 21:49:50 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = 
+{
+    env:{
+        API_KEY: "blte685dad492ce6905",
+        DELIVERY_TOKEN: "csf316fbd9a21cfb91b7b31960",
+        ENVIRONMENT_NAME: "development"
+    }
+    
+}
---
+++ stack/index.js
@@ -0,0 +1,3 @@
+import Contentstack from "contentstack;"
+const Stack = Contentstack.Stack(process.env.API_KEY,process.env.DELIVERY_TOKEN, process.env.ENVIRONMENT_NAME)
+export default Stack;
