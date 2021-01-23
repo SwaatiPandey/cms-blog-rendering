8b8e3fc1d0e66544737e7991b2e77c88b58d571f
SwaatiPandey <swati.pandey@contentstack.com>
2021-01-23 11:58:47 +0530

    removed commented part and console.log

--- components/footer.js
+++ components/footer.js
@@ -4,20 +4,11 @@ import {FaFacebook,FaTwitter,FaLinkedin} from "react-icons/fa";
 
 export default function Footer(props) {
   let footer = props.footer[0][0];
-  // console.log(footer);
+  
   return (
    
     <div className={styles.container}>
-      {/* <p>{footer.copyright_text} </p>
-      <div className={styles.socialIcons}>
-       {footer.social_media.map((link) => {
-          return (
-            <Link href={link.href}>
-              <p>{link.title}</p>
-            </Link>
-          );
-        })}
-      </div> */}
+     
       <div className = {styles.footer}>
          <img src="http://img.oasgames.com/upload/1484635542.png" alt="load" srcset=""/>
          <div className={styles.linksSec}> <h2>Link</h2> <li>Home</li> </div>
