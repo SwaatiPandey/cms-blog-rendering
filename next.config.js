f916306b7b42b60002f101e6d1b0e85923f275e5
SwaatiPandey <swati.pandey@contentstack.com>
2021-01-23 12:38:10 +0530

    modified gitignore

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = 
-{
-    env:{
-        API_KEY: "blte685dad492ce6905",
-        DELIVERY_TOKEN: "cs725c83dda69619af8e8c5a7e",
-        ENVIRONMENT_NAME: "development"
-    }
-    
-}
